15cc51f72ae276fafc870b4ff6dc6d526f2aefd3
Murilo <murilommms@gmail.com>
2020-01-25 20:37:15 -0300

    Adding Spring Boot application skeleton

---
+++ testes/backend-developer/solution/places-manager/src/test/java/com/clickbus/placesmanager/PlacesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clickbus.placesmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacesManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
