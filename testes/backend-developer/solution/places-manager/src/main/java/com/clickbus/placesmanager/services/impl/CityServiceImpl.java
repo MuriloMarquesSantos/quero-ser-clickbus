afd0a40249154008b26f3688248fa641360da512
murilo.santos <murilo.santos@wirecard.com>
2020-01-27 16:47:32 -0300

    Implementing City repository and adding injected attribute in service

--- testes/backend-developer/solution/places-manager/src/main/java/com/clickbus/placesmanager/services/impl/CityServiceImpl.java
+++ testes/backend-developer/solution/places-manager/src/main/java/com/clickbus/placesmanager/services/impl/CityServiceImpl.java
@@ -1,11 +1,16 @@
 package com.clickbus.placesmanager.services.impl;
 
 import com.clickbus.placesmanager.application.responsemodels.CityResponseModel;
+import com.clickbus.placesmanager.repository.CityRepository;
 import com.clickbus.placesmanager.services.CityService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@AllArgsConstructor
 @Service
 public class CityServiceImpl implements CityService {
+
+    private CityRepository cityRepository;
 
     @Override
     public CityResponseModel createCity() {
