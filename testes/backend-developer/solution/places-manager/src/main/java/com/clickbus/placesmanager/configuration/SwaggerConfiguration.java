92ebba5e4ea2d54d50aad6766d0b264fd0ae7450
murilo.santos <murilo.santos@wirecard.com>
2020-01-30 18:43:58 -0300

    Adding swagger configuration

---
+++ testes/backend-developer/solution/places-manager/src/main/java/com/clickbus/placesmanager/configuration/SwaggerConfiguration.java
@@ -0,0 +1,44 @@
+package com.clickbus.placesmanager.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfiguration {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .build()
+                .pathMapping("/")
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        Contact contact = new Contact(
+                "Murilo Santos",
+                "https://github.com/MuriloMarquesSantos",
+                "murilommms@gmail.com");
+
+        return new ApiInfo(
+                "Places Manager API",
+                "This API was created for Click-bus Back-end code Challenge." +
+                        "With it, you will be able to create and manage new places",
+                "1.0",
+                "",
+                contact,
+                "",
+                "",
+                new ArrayList<>());
+    }
+}
